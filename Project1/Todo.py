0e750091bfc5ae05562153746cddc3bd97ed9084
Amanuel <amanuelbehailu557@gmail.com>
2025-07-14 14:40:29 +0300

    To do

---
+++ Project1/Todo.py
@@ -0,0 +1,28 @@
+def intro():
+ print("Welcome to The To-Do List")
+ print ("1. Add a Task")
+ print ("2. List the Tasks of Today")
+ print ("3. Delete a Task")
+ print ("4. Mark as Done")
+ print ("5. Quit")
+
+ choice = input("Choose which One you want to Do from the above")
+
+ if choice == 1:
+  def task_writer():
+   task = input("What is the Task you want to add - ")
+   Lists=[]
+   Lists.append(task)
+  
+ 
+   
+
+
+
+
+
+
+def main():
+ intro()
+  
+main()
